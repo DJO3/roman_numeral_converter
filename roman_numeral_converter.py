9a952a70fd15beb819d5c97dc35a550594b175d2
Dave O'Connor <DJO3@users.noreply.github.com>
2015-05-14 22:20:48 -0400

    minor adjustments

--- roman_numeral_converter.py
+++ roman_numeral_converter.py
@@ -2,23 +2,23 @@ def convert(roman_numeral_string):
     nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     total = 0
     
-    for pos in range(len(data)):
-        numeral = data[pos]
+    for pos in range(len(roman_numeral_string)):
+        numeral = roman_numeral_string[pos]
         if pos == 0:
             total += nums[numeral]
-        elif pos != len(data)-1:
-            last_numeral = data[pos-1]
-            next_numeral = data[pos+1]
+        elif pos != len(roman_numeral_string)-1:
+            last_numeral = roman_numeral_string[pos-1]
+            next_numeral = roman_numeral_string[pos+1]
             if nums[numeral] > nums[last_numeral]:
                 total += nums[numeral]
                 total -= nums[last_numeral] * 2
             else:
                 total += nums[numeral]
         else:
-            if numeral in ['I', 'V']:
+            last_numeral = roman_numeral_string[pos-1]
+            if numeral in ['I', 'V', 'X'] and nums[numeral] < nums[last_numeral]:
                 total += nums[numeral]
             else:
-                last_numeral = data[pos-1]
                 total += nums[numeral]
                 total -= nums[last_numeral] * 2
     return total

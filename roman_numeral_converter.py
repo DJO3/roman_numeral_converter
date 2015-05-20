3c80b520cbda5adae9769a03be0e8f65c67e813e
Dave O'Connor <DJO3@users.noreply.github.com>
2015-05-20 16:26:10 -0400

    added roman_generator function

--- roman_numeral_converter.py
+++ roman_numeral_converter.py
@@ -1,3 +1,40 @@
+# Generates roman numeral from integer
+def roman_generator(integer):
+    nums = [('M', 1000), ('D', 500), ('C', 100), ('L', 50), ('X', 10), ('V', 5), ('I', 1)]
+    numeral = str()
+    
+    # Converts integer to value based roman numeral
+    while integer > 0:
+        for roman, value in nums:
+            if integer - value >= 0:
+                integer -= value
+                numeral = numeral + roman
+                break
+
+    # Repairs invalid sequence 4 in a row
+    for item in range(len(nums)):
+        roman = nums[item][0]
+        last_roman = nums[item-1][0]
+        if numeral.count(roman) == 4:
+            numeral = numeral.replace(roman*4, roman + last_roman)
+
+    # Repairs invalid sequence Large-Small-Large
+    for item in range(len(numeral)):
+        if item >= 2 and item < len(numeral):
+            first = numeral[item-2]
+            second = numeral[item-1]
+            third = numeral[item]
+            if first == third:
+                val = dict(nums)[first] * 2
+                for letter in nums:
+                    if letter[1] == val:
+                        roman = letter[0]
+                        numeral = numeral.replace(first + second + third, second + roman)
+
+    return numeral
+
+
+# Converts roman numeral to integer
 def convert(roman_numeral_string):
     nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     total = 0
